ffd28da14494e6eeb813fbd9baebe765ab3257d5
Manuel <carlosd99m@gmail.com>
2021-03-08 15:03:49 -0600

    routerNavigate :art:

--- src/app/pages/crear-paciente/crear-paciente.component.ts
+++ src/app/pages/crear-paciente/crear-paciente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 import { PacientesService } from "../../services/pacientes.service";
 
 @Component({
@@ -7,14 +8,16 @@ import { PacientesService } from "../../services/pacientes.service";
   styleUrls: ["./crear-paciente.component.css"],
 })
 export class CrearPacienteComponent {
+
   pacientes: any = {};
 
-  constructor(private pacientesServices: PacientesService) {}
+  constructor(private pacientesServices: PacientesService, private router: Router) {}
 
   AltaPaciente() {
     this.pacientesServices.altaPaciente(this.pacientes).subscribe(resp => {
       if ((resp["resultado"] = "OK")) {
         console.log("Resgistro exitoso");
+        this.router.navigate(['/nuevo-historial']); // Esto nos manda a crear el historial del paciente
       }
     });
   }
